1be6868a42e17ccbdf5f2c41bf4bfb2fb4d3ed07
Muhammed Thasneem P Android <muhammed.thasneem@yahoo.com>
2017-07-22 13:45:33 +0530

    Files done

---
+++ Workshoop/app/src/main/java/app/demo/muhammed/workshoop/LoginActivity.java
@@ -0,0 +1,13 @@
+package app.demo.muhammed.workshoop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
